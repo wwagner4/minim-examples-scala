85ea1c360189ee763029d2ee344c1406bddde61d
wwagner4 <wwagner4@gmail.com>
2016-06-04 16:15:17 +0200

    sinewave playing

--- src/main/scala/Glisando.scala
+++ src/main/scala/Glisando.scala
@@ -1,8 +1,9 @@
 import java.io.{File, FileInputStream, InputStream}
 
-import ddf.minim.Minim
+import ddf.minim.{AudioOutput, Minim}
 import ddf.minim.javasound.JSMinim
-import ddf.minim.spi.MinimServiceProvider
+import ddf.minim.spi.{AudioOut, MinimServiceProvider}
+import ddf.minim.ugens.{ADSR, Instrument, Oscil, Waves}
 
 object Glisando extends App {
 
@@ -12,7 +13,7 @@ object Glisando extends App {
   val out = minim.getLineOut()
 
   out.pauseNotes()
-  out.playNote(400)
+  out.playNote(0, 1.5f, Inst(out))
   out.resumeNotes()
 
   closeAfter(3)
@@ -24,6 +25,25 @@ object Glisando extends App {
   }
 
 
+  case class Inst(out: AudioOutput) extends Instrument {
+
+    val oscil = new Oscil(333, 0.3f, Waves.SINE)
+    val adsr = new ADSR(1f, 0.1f, 0.0f, 1f, 0.5f)
+
+
+    oscil.patch(adsr)
+
+    override def noteOn(duration: Float): Unit = {
+      adsr.patch(out)
+      adsr.noteOn()
+    }
+
+    override def noteOff(): Unit = {
+      adsr.noteOff()
+      adsr.unpatchAfterRelease(out)
+    }
+
+  }
   case class FileLoaderUserHome(path: String) {
 
     def sketchPath(fileName: String) = {
@@ -47,6 +67,7 @@ object Glisando extends App {
       new File(outDir, fileName)
     }
   }
+
 
 
 }
